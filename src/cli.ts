60acf6a2bf5497f36e5870a67a960fb74f9cff9d
Nikita Filonov <nikita.filonov@dif.tech>
2025-05-04 20:16:47 +0300

    update command

--- src/cli.ts
+++ src/cli.ts
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .name('ui-coverage-scenario-tool')
   .description('UI Coverage Scenario CLI Tool')
-  .version('0.2.0');
+  .version('0.3.0');
 
 program
   .command('save-report')
